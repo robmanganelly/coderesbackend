9c5b7d60104a5f4424e2e42726d96368e51a7415
robmanganelly <robmanganelly@gmail.com>
2021-12-17 17:11:21 -0500

    fixing bug on post solution

--- middleware/validators.js
+++ middleware/validators.js
@@ -49,7 +49,7 @@ module.exports.deferredProblemBodyValidator = catchAsync(async (req, res, next)=
 });
 
 module.exports.solutionBodyValidator = catchAsync(async(req, res, next)=>{
-    minlengthValidator(req.body.text,5);
-    maxlengthValidator(req.body.text,3500);
+    minlengthValidator(req.body.solution,5);
+    maxlengthValidator(req.body.solution,3500);
     next();
 });
