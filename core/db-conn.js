dcea966d04313aec933fdc7d91da48f5c889e8d4
robmanganelly <robmanganelly@gmail.com>
2022-01-09 17:24:03 -0500

    fixing db connection

--- core/db-conn.js
+++ core/db-conn.js
@@ -1,11 +1,15 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(
-    process.env.NODE_ENV === 'production' ? 
-        // process.env.DB_CONN_STRING : 
-        `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.db_CLUSTER_URL}/codetricksdb?retryWrites=true&w=majority`:
-        'mongodb://localhost:27017/codetricksdb',{ // important: if you clone this repo you must add your own connection string
-        useNewUrlParser: true,
+const conn_string  = process.env.NODE_ENV === "production" ? 
+`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.db_CLUSTER_URL}/codetricksdb?retryWrites=true&w=majority`:
+'mongodb://localhost:27017/codetricksdb' 
+
+console.log(conn_string);
+
+mongoose.connect( conn_string,
+    // process.env.DB_CONN_STRING  // this was replaced 
+    {
+            useNewUrlParser: true,
     }// todo replace this console.log with proper logger service.
 ).then(
     () => {
